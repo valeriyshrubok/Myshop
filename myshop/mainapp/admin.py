a865835192a43c4ca492860a5cda9362e94c6ea4
valeriyshrubok <valerarabota5595@gmail.com>
2021-05-31 23:41:14 +0300

    add NotebookAdminForm

--- myshop/mainapp/admin.py
+++ myshop/mainapp/admin.py
@@ -1,10 +1,29 @@
-from django.forms import ModelChoiceField
+from django.core.exceptions import ValidationError
+from django.forms import ModelChoiceField, ModelForm
 from django.contrib import admin
 from .models import *
+from PIL import Image
 
+class NotebookAdminForm(ModelForm):
+
+    VALID_RESOLUTION = (400, 400)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = f"Загружайте изображение с минимальным разрешением {self.VALID_RESOLUTION}"
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_width, min_height = self.VALID_RESOLUTION
+        if img.height < min_height or img.widht < min_width:
+            raise ValidationError('Разрешение изображения меньше минимального!')
+        return image
 
 
 class NotebookAdmin(admin.ModelAdmin):
+
+    form = NotebookAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
