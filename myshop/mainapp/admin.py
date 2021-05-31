b7ffd10d94bd8e265048fe88ec4fa8edcc514a60
valeriyshrubok <valerarabota5595@gmail.com>
2021-05-31 18:43:47 +0300

    add admin.py

--- myshop/mainapp/admin.py
+++ myshop/mainapp/admin.py
@@ -1,3 +1,29 @@
+from django.forms import ModelChoiceField
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+
+class NotebookAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='notebooks'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class SmartphoneAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='smartphones'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Category)
+# admin.site.register(Product)
+admin.site.register(Notebook, NotebookAdmin)
+admin.site.register(Smartphone, SmartphoneAdmin)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
