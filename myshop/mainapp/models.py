5088409a27531eec663936179a9eb430d540fa5c
valeriyshrubok <valerarabota5595@gmail.com>
2021-05-31 21:27:34 +0300

    update models with_respect_to

--- myshop/mainapp/models.py
+++ myshop/mainapp/models.py
@@ -5,6 +5,30 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 
 User = get_user_model()
 
+class LatestProductsManager:
+
+    @staticmethod
+    def get_products_for_mainpage(*args, **kwargs):
+        with_respect_to = kwargs.get('with_respect_to')
+        products = []
+        ct_models = ContentType.objects.filter(model__in=args)
+        for ct_model in ct_models:
+            model_products = ct_model.model_class()._base_manager.all().order_by('id')[:5]
+            products.extend(model_products)
+        if with_respect_to:
+            ct_model = ContentType.objects.filter(model=with_respect_to)
+            if ct_model.exists():
+                if with_respect_to in args:
+                    return sorted(
+                        products, key=lambda x: x.__class__._meta.model_name.startswith(with_respect_to), reverse=True
+                    )
+        return products
+
+
+class LatestProducts:
+
+    objects = LatestProductsManager
+
 class Category(models.Model):
 
     name = models.CharField(max_length=200, verbose_name='Имя категории')
