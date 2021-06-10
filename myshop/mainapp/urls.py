9bc2b7500b0c8559d512844a3357208fa56a8dfc
valeriyshrubok <valerarabota5595@gmail.com>
2021-06-10 16:18:27 +0300

    add checkout.html and register in urls,views

--- myshop/mainapp/urls.py
+++ myshop/mainapp/urls.py
@@ -7,7 +7,8 @@ from .views import (
     CartView,
     AddToCartView,
     DeleteFromCartView,
-    ChangeQuantityView
+    ChangeQuantityView,
+    CheckoutView
 )
 
 urlpatterns = [
@@ -17,5 +18,6 @@ urlpatterns = [
     path('cart/', CartView.as_view(), name='cart'),
     path('add-to-cart/<str:ct_model>/<str:slug>/', AddToCartView.as_view(), name='add_to_cart'),
     path('remove-from-cart/<str:ct_model>/<str:slug>/', DeleteFromCartView.as_view(), name='delete_from_cart'),
-    path('change-quantity/<str:ct_model>/<str:slug>/', ChangeQuantityView.as_view(), name='change_quantity')
+    path('change-quantity/<str:ct_model>/<str:slug>/', ChangeQuantityView.as_view(), name='change_quantity'),
+    path('checkout/', CheckoutView.as_view(), name='checkout')
 ]
