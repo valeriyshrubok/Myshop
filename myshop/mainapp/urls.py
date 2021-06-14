a018710aeb7281fc3427e8dc5b897479a591ed84
valeriyshrubok <valerarabota5595@gmail.com>
2021-06-14 14:56:37 +0300

    add Authentication

--- myshop/mainapp/urls.py
+++ myshop/mainapp/urls.py
@@ -9,7 +9,8 @@ from .views import (
     DeleteFromCartView,
     ChangeQuantityView,
     CheckoutView,
-    MakeOrderView
+    MakeOrderView,
+    LoginView
 )
 
 urlpatterns = [
@@ -21,5 +22,6 @@ urlpatterns = [
     path('remove-from-cart/<str:slug>/', DeleteFromCartView.as_view(), name='delete_from_cart'),
     path('change-quantity/<str:slug>/', ChangeQuantityView.as_view(), name='change_quantity'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
-    path('make-order', MakeOrderView.as_view(), name='make_order')
+    path('make-order', MakeOrderView.as_view(), name='make_order'),
+    path('login/', LoginView.as_view(), name='login')
 ]
--- myshop/mainapp/views.py
+++ myshop/mainapp/views.py
@@ -2,11 +2,12 @@ from django.db import transaction
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
 from django.views.generic import DetailView, View
 from .models import Category, Customer, Cart, CartProduct, Product
 from .mixins import CartMixin
-from .forms import OrderForm
+from .forms import OrderForm, LoginForm
 from .utils import recalculation_cart
 
 
@@ -156,3 +157,22 @@ class MakeOrderView(CartMixin, View):
             messages.add_message(request, messages.INFO, 'Заказ оформлен! Наш Менеджер с вами свяжется')
             return HttpResponseRedirect('/')
         return HttpResponseRedirect('/checkout/')
+
+class LoginView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        form = LoginForm(request.POST or None)
+        categories = Category.objects.all()
+        context = {'form': form, 'categories': categories, 'cart': self.cart}
+        return render(request, 'login.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST or None)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        return render(request, 'login.html', {'form': form, 'cart': self.cart})
